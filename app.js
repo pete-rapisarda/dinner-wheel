6feab38d14896a5a1634e62810d8e480615e52c1
Pete Rapisarda <pete@robopete.com>
2025-05-09 06:06:45 -0400

    added javascript to capture and display restaurant list #2

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
-// Placeholder for future JavaScript logic
 console.log("Dinner Wheel App initialized");
+
+const addButton = document.getElementById('add-restaurant');
+const restaurantInput = document.getElementById('restaurant-name');
+const restaurantListDiv = document.getElementById('restaurant-list');
+
+let restaurants = [
+    "Italian", "Chinese", "Mexican", "Thai", "American",
+    "Mediterranean", "Indian", "Japanese", "Korean", "Greek", "BBQ", "Seafood", "Vegan", "Fast Food"
+]
+
+updateRestaurantList();
+
+addButton.addEventListener('click',function() {
+    const name = restaurantInput.value.trim();
+    if (name !== "") {
+        restaurants.push(name);
+        restaurantInput.value = "";
+        updateRestaurantList();
+    }
+});
+
+function updateRestaurantList() {
+    restaurantListDiv.innerHTML = "";
+    restaurants.forEach((restaurant, index) => {
+        const p = document.createElement('p');
+        p.textContent = `${index + 1}.${restaurant}`;
+        restaurantListDiv.appendChild(p);
+    })
+}
+
+restaurantInput.addEventListener('keypress',function(event){
+    if (event.key === "Enter") {
+        addButton.click();
+    }
+})
